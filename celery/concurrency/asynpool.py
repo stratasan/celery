cc8b076d98a3aaa366a975b3458330a8b222d91e
Scott Burns <scott@stratasan.com>
2018-06-13 07:48:09 -0500

    Hack up asyncpool
    Applying a patch based on https://github.com/celery/celery/issues/4185#issuecomment-373755206

--- celery/concurrency/asynpool.py
+++ celery/concurrency/asynpool.py
@@ -745,6 +745,7 @@ class AsynPool(_pool.Pool):
             except KeyError:
                 pass
         self.on_inqueue_close = on_inqueue_close
+        self.hub_remove = hub_remove
 
         def schedule_writes(ready_fds, total_write_count=[0]):
             # Schedule write operation to ready file descriptor.
@@ -1250,6 +1251,7 @@ class AsynPool(_pool.Pool):
             if queue:
                 for sock in (queue._reader, queue._writer):
                     if not sock.closed:
+                        self.hub_remove(sock)
                         try:
                             sock.close()
                         except (IOError, OSError):
